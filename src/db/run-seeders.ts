6ca595485acdfebcdc1f5017ee99880b95f85c4e
aeron-cap <aeroncaponpon.01@gmail.com>
2026-04-14 21:54:57 +0800

    chore: changes for ci

--- src/db/run-seeders.ts
+++ src/db/run-seeders.ts
@@ -1,17 +1,12 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { runSeeders, SeederOptions } from 'typeorm-extension';
-import AccountClassSeeder from './seeds/account_class.seeder';
-import { AccountClasses } from 'src/entity_types/entities/account_class.entity';
-import { CardNetworks } from 'src/entity_types/entities/card_network.entity';
-import CardNetworkSeeder from './seeds/card_network.seeder';
 
 (async () => {
   const options: DataSourceOptions & SeederOptions = {
     type: 'postgres',
     url: process.env.DB_URL,
-    entities: [AccountClasses, CardNetworks],
-
-    seeds: [AccountClassSeeder, CardNetworkSeeder],
+    entities: [],
+    seeds: [],
   };
 
   const dataSource = new DataSource(options);
